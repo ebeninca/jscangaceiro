ed2c5729689f395ace9f1e69723f965a74a1b68b
eduardo.beninca <eduardo.beninca@dataprev.gov.br>
2024-08-29 18:30:45 -0300

    pag. 119

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,7 +1,37 @@
 class NegociacaoController {
 
+    constructor() {
+        //é uma copia da função querySelector, por referência, mantendo o contexto (document). 
+        let $ = document.querySelector.bind(document);
+
+        // buscando os elementos
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
+    }
+
     adiciona(event) {
         event.preventDefault();
-        alert('Chamei ação no controller');
+        this._negociacoes.adiciona(this._criaNegociacao()); // modificação!
+        this._negociacoes.paraArray().length = 0;
+        console.log(this._negociacoes.paraArray());
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        // retorna uma instância de negociação
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
     }
 }
